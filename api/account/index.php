3be4e21f61e2151f916dd9a6874f037fa6cd1cad
Daniel James Dolor <estheryahe@gmail.com>
2022-05-13 12:35:29 +0800

    code cleaning /api/account/

--- api/account/index.php
+++ api/account/index.php
@@ -1,41 +1,35 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/actionloaddb.php' ?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/api/commons.php' ?>
 <?php
 
-$param = array_filter(
-    explode(
-        "/",
-        str_replace("/api/account/", "", $_SERVER['REQUEST_URI'])
-    )
-);
-
-$body = json_decode(file_get_contents('php://input'));
+$param = get_uri_params("/api/account/");
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
     if (sizeof($param) == 1) {
-        $query_res = $dbc->query("SELECT * FROM account WHERE username='${param[0]}'");
+        $query_res = db_query("SELECT * FROM account WHERE username='${param[0]}'");
 
         if ($query_res->rowCount() == 1)
-            $data = $query_res->fetchAll(PDO::FETCH_ASSOC)[0];
-        else $data = ["msg" => "not found."];
+            respond($query_res->fetchAll(PDO::FETCH_ASSOC)[0], 202);
+        else
+            respond(["msg" => $param[0] . " not found"], 404);
     } else
-        $data = ["msg" => "wrong use case"];
+        respond(["msg" => " wrong use case"], 403);
 } else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
 
     if (sizeof($param) == 1) {
         try {
-            $query_res = $dbc->query("SELECT * FROM account WHERE username='${param[0]}'");
-            if ($query_res->rowCount() > 0) {
-                $query_res = $dbc->query("DELETE FROM account WHERE username='${param[0]}'");
-                $data = ["msg" => "Successfully deleted ${param[0]}"];
+            if (
+                db_query("SELECT * FROM account WHERE username='${param[0]}'")->rowCount() > 0
+            ) {
+                db_query("DELETE FROM account WHERE username='${param[0]}'");
+                respond(["msg" => "Successfully deleted ${param[0]}"], 202);
             } else
-                $data = ["msg" => "${param[0]} not found"];
+                respond(["msg" => $param[0] . " not found"], 404);
         } catch (PDOException $e) {
-            $data = ["msg" => "${param[0]} has existing blogposts"];
+            respond(["msg" => "${param[0]} has existing blogposts"], 403);
         }
     } else
-        $data = ["msg" => "wrong use case"];
-}
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($data);
+        respond(["msg" => "wrong use case"], 400);
+} else
+    respond(["msg" => "HTTP method not allowed"], 501);
